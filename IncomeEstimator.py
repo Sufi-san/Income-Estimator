f64f4eccdf28efda3b1c538a6861be3cf99eadfe
Sufiyan <sufy_456@hotmail.com>
2023-04-04 15:46:00 +0530

    Basic Changes in GUI and data handling

--- IncomeEstimator.py
+++ IncomeEstimator.py
@@ -28,6 +28,9 @@ def open_create():
     def save_file():
         pass
 
+    def sort_emp_list():
+        pass
+
     def close_action():
         messagebox_toplevel = Toplevel(root)
         messagebox_toplevel.title("Invisible")
@@ -47,33 +50,61 @@ def open_create():
 
     def when_focused1(event):
         check = entry_4.get()
-        if check == placeholder_ent4a or check == placeholder_ent4b:
+        if check == placeholder_ent4a or check == placeholder_ent4b or check == "":
             event.widget.delete(0, END)
             entry_4.configure(fg="black")
+            if v2.get() == 1:
+                entry_4.configure(validatecommand=(validate1, "%P"))
+            elif v2.get() == 2:
+                entry_4.configure(validatecommand=(validate2, "%P"))
 
     def enable_prod():
         if v1.get() == 1:
             entry_5.configure(state=NORMAL)
+            entry_6.configure(state=NORMAL)
+            entry_7.configure(state=NORMAL)
+            entry_8.configure(state=NORMAL)
+            entry_9.configure(state=NORMAL)
             btn_7.configure(state=NORMAL)
             btn_8.configure(state=NORMAL)
             btn_9.configure(state=NORMAL)
             btn_10.configure(state=NORMAL)
-            cmb_box.configure(state="readonly")
+            btn_11.configure(state=NORMAL)
+            btn_12.configure(state=NORMAL)
+            btn_13.configure(state=NORMAL)
+            btn_14.configure(state=NORMAL)
+            cmb_box1.configure(state="readonly")
             lbl_2.configure(state=NORMAL)
             lbl_3.configure(state=NORMAL)
             lbl_9.configure(state=NORMAL)
             lbl_10.configure(state=NORMAL)
+            lbl_11.configure(state=NORMAL)
+            lbl_12.configure(state=NORMAL)
+            lbl_13.configure(state=NORMAL)
+            lbl_14.configure(state=NORMAL)
         elif v1.get() == 0:
             entry_5.configure(state=DISABLED)
+            entry_6.configure(state=DISABLED)
+            entry_7.configure(state=DISABLED)
+            entry_8.configure(state=DISABLED)
+            entry_9.configure(state=DISABLED)
             btn_7.configure(state=DISABLED)
             btn_8.configure(state=DISABLED)
             btn_9.configure(state=DISABLED)
             btn_10.configure(state=DISABLED)
-            cmb_box.configure(state=DISABLED)
+            btn_11.configure(state=DISABLED)
+            btn_12.configure(state=DISABLED)
+            btn_13.configure(state=DISABLED)
+            btn_14.configure(state=DISABLED)
+            cmb_box1.configure(state=DISABLED)
             lbl_2.configure(state=DISABLED)
             lbl_3.configure(state=DISABLED)
             lbl_9.configure(state=DISABLED)
             lbl_10.configure(state=DISABLED)
+            lbl_11.configure(state=DISABLED)
+            lbl_12.configure(state=DISABLED)
+            lbl_13.configure(state=DISABLED)
+            lbl_14.configure(state=DISABLED)
 
     def on_combo_select(event):
         global products
@@ -84,7 +115,7 @@ def open_create():
                 if len(product) > max_num:
                     max_num = products.index(product)
             width = font.measure(products[max_num])
-            if width > cmb_box.winfo_width():
+            if width > cmb_box1.winfo_width():
                 style = ttk.Style()
                 style.configure('TCombobox', postoffset=(0, 0, width, 0))
 
@@ -122,23 +153,24 @@ def open_create():
         global count_tree1, count_tree2, count_tree3
         global products
         if a == 1:
-            if cmb_box.get() != "" and entry_5.get() != "":
+            if cmb_box1.get() != "" and entry_5.get() != "":
                 t_profit = 0
                 for index in tree_view2.get_children():
                     tree2_list = tree_view2.item(index)["values"]
-                    print(tree2_list)
-                    if cmb_box.get() == tree2_list[0]:
-                        t_profit = (int(tree2_list[3]) - int(tree2_list[2])) * int(entry_5.get())
-                value = (cmb_box.get(), int(entry_5.get()), t_profit)
+                    if cmb_box1.get() == tree2_list[0]:
+                        t_profit = float(tree2_list[2]) * int(entry_5.get())
+                value = (cmb_box1.get(), int(entry_5.get()), t_profit)
                 tree_view1.insert(parent="", index="end", iid=str(count_tree1), text="", values=value)
                 count_tree1 += 1
             else:
                 fill_data_msg(1, "Data Incomplete", "Please fill all the required details.")
         if a == 2:
             if entry_6.get() != "" and entry_7.get() != "" and entry_8.get() != "" and entry_9.get() != "":
-                value = (entry_6.get(), entry_7.get(), entry_8.get(), entry_9.get())
+                margin = entry_7.get() + "% of Profit = " + str(round_to_3(float(entry_7.get()) * (float(entry_9.get()) - float(entry_8.get())) / 100))
+                profit = round_to_3(float(entry_9.get()) - float(entry_8.get()))
+                value = (entry_6.get(), margin, profit)
                 products.append(entry_6.get())
-                cmb_box.config(values=products)
+                cmb_box1.config(values=products)
                 tree_view2.insert(parent="", index="end", iid=str(count_tree2), text="", values=value)
                 count_tree2 += 1
             else:
@@ -165,13 +197,13 @@ def open_create():
         if a == 1:
             records = tree_view1.selection()
             for record in records:
-                if cmb_box.get() != "" and entry_5.get() != "":
+                if cmb_box1.get() != "" and entry_5.get() != "":
                     t_profit = 0
                     for index in tree_view2.get_children():
                         tree2_list = tree_view2.item(index)["values"]
-                        if cmb_box.get() == tree2_list[0]:
-                            t_profit = (int(tree2_list[3]) - int(tree2_list[2])) * int(entry_5.get())
-                    value = (cmb_box.get(), int(entry_5.get()), t_profit)
+                        if cmb_box1.get() == tree2_list[0]:
+                            t_profit = float(tree2_list[2]) * int(entry_5.get())
+                    value = (cmb_box1.get(), int(entry_5.get()), t_profit)
                     tree_view1.item(record, values=value)
                 else:
                     fill_data_msg(1, "Data Incomplete", "Please fill all the required details.")
@@ -181,10 +213,12 @@ def open_create():
                 old_value = tree_view2.item(record)["values"][0]
                 upd_idx = products.index(old_value)
                 if entry_6.get() != "" and entry_7.get() != "" and entry_8.get() != "" and entry_9.get() != "":
-                    value = (entry_6.get(), entry_7.get(), entry_8.get(), entry_9.get())
+                    margin = entry_7.get() + "% of Profit = " + str(round_to_3(float(entry_7.get())*(float(entry_9.get()) - float(entry_8.get()))/100))
+                    profit = round_to_3(float(entry_9.get()) - float(entry_8.get()))
+                    value = (entry_6.get(), margin, profit)
                     tree_view2.item(record, values=value)
                     products[upd_idx] = entry_6.get()
-                    cmb_box.config(values=products)
+                    cmb_box1.config(values=products)
                 else:
                     fill_data_msg(1, "Data Incomplete", "Please fill all the required details.")
         if a == 3:
@@ -217,7 +251,7 @@ def open_create():
                 deletable = tree_view2.item(record)["values"][0]
                 tree_view2.delete(record)
                 products.remove(deletable)
-                cmb_box.config(values=products)
+                cmb_box1.config(values=products)
         if a == 3:
             records = tree_view3.selection()
             for record in records:
@@ -231,35 +265,49 @@ def open_create():
         if a == 2:
             for record in tree_view2.get_children():
                 products.clear()
-                cmb_box.config(values=products)
+                cmb_box1.config(values=products)
                 tree_view2.delete(record)
         if a == 3:
             for record in tree_view3.get_children():
                 tree_view3.delete(record)
 
+    def round_to_3(data):
+        return round(data, 3)
+
+    def validate_if_float(new_value):
+        if new_value == "":
+            return True
+        try:
+            float(new_value)
+            if new_value.count(".") > 1:
+                return False
+            return True
+        except ValueError:
+            return False
+
+    def validate_percent(new_value):
+        if new_value == "" or new_value == placeholder_ent4a or new_value == placeholder_ent4b:
+            return True
+        try:
+            float_val = float(new_value)
+            if new_value.count(".") > 1:
+                return False
+            elif float_val < 1 or float_val > 100:
+                fill_data_msg(2, "Out of Limit", "Percentage can be between 1 to 100 only.")
+                return False
+            return True
+        except ValueError:
+            return False
+
     def validate_if_num(new_value):
         if new_value.isdigit() or new_value == "":
-            return True
-        elif new_value == "\b":
             return True
         else:
             return False
 
-    def validate_percent(new_value):
-        if new_value.isdigit() or new_value == "" or new_value == placeholder_ent4a or new_value == placeholder_ent4b:
-            if new_value == "" or new_value == placeholder_ent4a or new_value == placeholder_ent4b:
-                return True
-            else:
-                value = int(new_value)
-                if 1 <= value <= 100:
-                    return True
-                else:
-                    fill_data_msg(2, "Out of Limit", "Percentage can be between 1 to 100 only.")
-                    return False
-        elif new_value == "\b":
-            return True
-        else:
-            return False
+    def on_child_min(event):
+        if event.widget == child:
+            root.deiconify()
 
     global window_counter1
     global products
@@ -271,10 +319,12 @@ def open_create():
         child.resizable(False, False)
         child.geometry(f"{child.winfo_screenwidth()}x{child.winfo_screenheight()}+-7+0")
         child.protocol("WM_DELETE_WINDOW", close_action)
+        child.bind("<Unmap>", on_child_min)
         child.iconbitmap("app_icon.ico")
 
-        validate1 = child.register(validate_if_num)
+        validate1 = child.register(validate_if_float)
         validate2 = child.register(validate_percent)
+        validate3 = child.register(validate_if_num)
 
         frame1 = LabelFrame(child, bg="#b4b4b4", text="Employee Details")
         frame1.place(x=40, y=110, width=653, height=280)
@@ -312,16 +362,14 @@ def open_create():
         scr1b.config(command=tree_view1.xview)
 
         tree_view2 = ttk.Treeview(tree_frame2, yscrollcommand=scr2a.set, xscrollcommand=scr2b.set)
-        tree_view2["columns"] = ("Product Name", "Employee Margin", "Cost Price", "Selling Price")
+        tree_view2["columns"] = ("Product Name", "Employee Margin", "Profit")
         tree_view2.column("#0", width=0, stretch=NO)
         tree_view2.column("Product Name", anchor=W, width=140)
-        tree_view2.column("Employee Margin", anchor=W, width=120)
-        tree_view2.column("Cost Price", anchor=W, width=120)
-        tree_view2.column("Selling Price", anchor=W, width=120)
+        tree_view2.column("Employee Margin", anchor=W, width=160)
+        tree_view2.column("Profit", anchor=W, width=120)
         tree_view2.heading("Product Name", text="Product Name", anchor=W)
         tree_view2.heading("Employee Margin", text="Employee Margin", anchor=W)
-        tree_view2.heading("Cost Price", text="Cost Price", anchor=W)
-        tree_view2.heading("Selling Price", text="Selling Price", anchor=W)
+        tree_view2.heading("Profit", text="Profit", anchor=W)
         tree_view2.pack(side=LEFT, expand=True, fill=BOTH)
         scr2a.config(command=tree_view2.yview)
         scr2b.config(command=tree_view2.xview)
@@ -348,13 +396,13 @@ def open_create():
         btn_2 = Button(child, text="Estimate Incomes", font=("Times New Roman", 22, "bold"), command=estimate_inc)
         btn_2.place(x=990, y=390, width=234, height=52)
         btn_3 = Button(child, text="Add Record", command=lambda: add_rec_tree(3))
-        btn_3.place(x=50, y=350, width=129, height=30)
+        btn_3.place(x=50, y=350, width=119, height=30)
         btn_4 = Button(child, text="Update Selected", command=lambda: upd_rec_tree(3))
-        btn_4.place(x=190, y=350, width=129, height=30)
+        btn_4.place(x=178, y=350, width=119, height=30)
         btn_5 = Button(child, text="Remove Selected", command=lambda: rem_select_rec_tree(3))
-        btn_5.place(x=330, y=350, width=129, height=30)
+        btn_5.place(x=306, y=350, width=119, height=30)
         btn_6 = Button(child, text="Remove All", command=lambda: rem_all_rec_tree(3))
-        btn_6.place(x=470, y=350, width=129, height=30)
+        btn_6.place(x=435, y=350, width=119, height=30)
         btn_7 = Button(child, text="Add Record", state=DISABLED, command=lambda: add_rec_tree(1))
         btn_7.place(x=540, y=220, width=120, height=20)
         btn_8 = Button(child, text="Update Selected", state=DISABLED, command=lambda: upd_rec_tree(1))
@@ -363,18 +411,21 @@ def open_create():
         btn_9.place(x=540, y=280, width=120, height=20)
         btn_10 = Button(child, text="Remove All", state=DISABLED, command=lambda: rem_all_rec_tree(1))
         btn_10.place(x=540, y=310, width=120, height=20)
-        btn_11 = Button(child, text="Add Product", command=lambda: add_rec_tree(2))
+        btn_11 = Button(child, text="Add Product", state=DISABLED, command=lambda: add_rec_tree(2))
         btn_11.place(x=760, y=200, width=211, height=30)
-        btn_12 = Button(child, text="Update Selected Products", command=lambda: upd_rec_tree(2))
+        btn_12 = Button(child, text="Update Selected Products", state=DISABLED, command=lambda: upd_rec_tree(2))
         btn_12.place(x=760, y=240, width=211, height=30)
-        btn_13 = Button(child, text="Remove Selected Products", command=lambda: rem_select_rec_tree(2))
+        btn_13 = Button(child, text="Remove Selected Products", state=DISABLED, command=lambda: rem_select_rec_tree(2))
         btn_13.place(x=760, y=280, width=211, height=30)
-        btn_14 = Button(child, text="Remove All", command=lambda: rem_all_rec_tree(2))
+        btn_14 = Button(child, text="Remove All", state=DISABLED, command=lambda: rem_all_rec_tree(2))
         btn_14.place(x=760, y=320, width=211, height=30)
+        btn_15 = Button(child, text="Apply Sort", command=sort_emp_list)
+        btn_15.place(x=570, y=365, width=102, height=18)
 
         v1 = IntVar()
         chk_btn1 = Checkbutton(child, text="Include Products", variable=v1, offvalue=0, onvalue=1, command=enable_prod)
-        chk_btn1.place(x=420, y=0, width=208, height=38)
+        chk_btn1["font"] = ("Calibre", 15, "bold")
+        chk_btn1.place(x=420, y=10, width=218, height=38)
 
         v2 = IntVar()
         rad_btn1 = Radiobutton(child, text="Fixed Amount", bg="#b4b4b4", variable=v2, value=1, command=choose)
@@ -402,13 +453,13 @@ def open_create():
         lbl_9.place(x=520, y=130, width=70, height=25)
         lbl_10 = Label(child, text="Quantity:", font=f, bg="#b4b4b4", state=DISABLED)
         lbl_10.place(x=520, y=170, width=70, height=25)
-        lbl_11 = Label(child, text="Product Name:", font=f, bg="#b4b4b4")
+        lbl_11 = Label(child, text="Product Name:", state=DISABLED, font=f, bg="#b4b4b4")
         lbl_11.place(x=740, y=60, width=108, height=37)
-        lbl_12 = Label(child, text="Employee Margin:", font=f, bg="#b4b4b4")
+        lbl_12 = Label(child, text="Employee Margin:", state=DISABLED, font=f, bg="#b4b4b4")
         lbl_12.place(x=740, y=90, width=126, height=38)
-        lbl_13 = Label(child, text="Cost Price:", font=f, bg="#b4b4b4")
+        lbl_13 = Label(child, text="Cost Price:", font=f, state=DISABLED, bg="#b4b4b4")
         lbl_13.place(x=740, y=120, width=83, height=36)
-        lbl_14 = Label(child, text="Selling Price:", font=f, bg="#b4b4b4")
+        lbl_14 = Label(child, text="Selling Price:", font=f, state=DISABLED, bg="#b4b4b4")
         lbl_14.place(x=740, y=150, width=96, height=39)
         lbl_15 = Label(child, text="Amount left after Distribution:", font=("Roboto", 13, "bold"))
         lbl_15.place(x=990, y=480, width=262, height=30)
@@ -425,25 +476,30 @@ def open_create():
         entry_2.place(x=120, y=140, width=140, height=20)
         entry_3 = Entry(child, justify=CENTER)
         entry_3.place(x=120, y=180, width=140, height=20)
-        entry_4 = Entry(child, fg="#b4b4b4", justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate2, "%P"))
+        entry_4 = Entry(child, fg="#b4b4b4", justify=CENTER, state=DISABLED, validate="key")
         placeholder_ent4a = "Enter Amount"
         placeholder_ent4b = "Enter % Collection"
         entry_4.bind("<FocusIn>", when_focused1)
         entry_4.place(x=120, y=290, width=135, height=30)
-        entry_5 = Entry(child, justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate1, "%P"))
+        entry_5 = Entry(child, justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate3, "%P"))
         entry_5.place(x=590, y=170, width=79, height=30)
-        entry_6 = Entry(child, justify=CENTER)
+        entry_6 = Entry(child, justify=CENTER, state=DISABLED)
         entry_6.place(x=860, y=67, width=125, height=20)
-        entry_7 = Entry(child, justify=CENTER, validate="key", validatecommand=(validate2, "%P"))
+        entry_7 = Entry(child, justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate2, "%P"))
         entry_7.place(x=860, y=100, width=125, height=20)
-        entry_8 = Entry(child, justify=CENTER, validate="key", validatecommand=(validate1, "%P"))
+        entry_8 = Entry(child, justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate1, "%P"))
         entry_8.place(x=860, y=130, width=125, height=20)
-        entry_9 = Entry(child, justify=CENTER, validate="key", validatecommand=(validate1, "%P"))
+        entry_9 = Entry(child, justify=CENTER, state=DISABLED, validate="key", validatecommand=(validate1, "%P"))
         entry_9.place(x=860, y=160, width=125, height=20)
 
-        cmb_box = ttk.Combobox(child, values=[], state=DISABLED)
-        cmb_box.place(x=590, y=130, width=80, height=25)
-        cmb_box.bind("<Button-1>", on_combo_select)
+        cmb_box1 = ttk.Combobox(child, values=[], state=DISABLED)
+        cmb_box1.place(x=590, y=130, width=80, height=25)
+        cmb_box1.bind("<Button-1>", on_combo_select)
+        sort_list = ["ID (Ascending)", "ID (Descending)", "Name (Ascending)", "Name (Descending)", "Salary (Ascending)", "Salary (Descending)", "Product (Ascending)", "Product (Descending)", "Income (Ascending)", "Income (Descending)"]
+        cmb_box2 = ttk.Combobox(child, values=sort_list, state="readonly")
+        cmb_box2.place(x=569, y=345, width=116, height=17)
+
+        child.mainloop()
     else:
         messagebox.showinfo("Limit reached!", "Can Open Only One Window")
 
